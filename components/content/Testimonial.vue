f65c61de6e06da9bf927e1509cb685f0a6866970
Damien Roche <damienroche@free.fr>
2023-03-02 18:13:03 +0100

    fix: testimonial image (#5)

--- components/content/Testimonial.vue
+++ components/content/Testimonial.vue
@@ -23,7 +23,7 @@ defineProps({
         <p>“{{ quote }}”</p>
       </blockquote>
       <figcaption class="mt-6 flex items-center gap-x-4">
-        <NuxtImg class="h-10 w-10 rounded-full bg-zinc-50 dark:bg-zinc-800" :src="image?.src ? image.src : '/img/placeholder.jpg'" :alt="image?.alt ? image.alt : 'Placeholder'" :width="image?.width ? image.width : 80" :height="image?.height ? image.height : 80" />
+        <NuxtImg class="h-10 w-10 rounded-full bg-zinc-50 dark:bg-zinc-800 object-cover" :src="image?.src ? image.src : '/img/placeholder.jpg'" :alt="image?.alt ? image.alt : 'Placeholder'" :width="image?.width ? image.width : 80" :height="image?.height ? image.height : 80" />
         <div class="font-semibold text-zinc-900 dark:text-zinc-300">{{ name }}</div>
       </figcaption>
     </figure>
